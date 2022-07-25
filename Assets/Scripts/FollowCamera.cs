58636575d3fdb7fded9b27755100af417d965af5
Aaron S Grincewicz <veganimus@outlook.com>
2022-07-25 18:08:31 -0400

    follow camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+   private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,20 +1,34 @@
 using UnityEngine;
 using UnityEngine.AI;
+
 [RequireComponent(typeof(NavMeshAgent))]
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
     private NavMeshAgent _navMeshAgent;
-    
+    private Camera _mainCamera;
+
     private void Start()
-    {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
-    }
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+            _mainCamera = Camera.main;
+        }
     
     private void Update()
     {
-        if (_navMeshAgent.destination == _target.position) return;
-        _navMeshAgent.SetDestination(_target.position);
-        //Debug.Log("Agent Set");
+        if(Input.GetMouseButtonDown(0))
+        {
+           MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        bool hasHit = Physics.Raycast(ray, out hitInfo);
+        if (hasHit)
+        {
+            _navMeshAgent.SetDestination(hitInfo.point);
+        }
     }
 }

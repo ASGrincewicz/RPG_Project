d7902e992877d2eca49b3e7df78fffc5c3d5e4cf
Aaron S Grincewicz <veganimus@outlook.com>
2022-08-07 19:34:54 -0400

    AI Patrol path.

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -8,8 +8,19 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] private float _chaseDistance = 3.0f;
+        [SerializeField] private float _suspicionWaitTime = 3.0f;
+        [Header("Patrol Configuration")]
+        [SerializeField] private PatrolPath _patrolPath = null;
+        [SerializeField] private float _waypointTolerance = 1.0f;
+        [SerializeField] private float _waypointDwellTime = 2.0f;
+        
 
         private Vector3 _guardPosition;
+        private int _currentWaypointIndex = 0;
+
+        private float _timeSinceLastSawPlayer = Mathf.Infinity;
+
+        private float _timeAtWaypoint = Mathf.Infinity;
         //Cached References
         private Fighter _fighter;
         private Mover _mover;
@@ -45,13 +56,80 @@ namespace RPG.Control
             {
                 if (_fighter.CanAttack(_target))
                 {
-                    _fighter.Attack(_target);
+                    AttackBehavior();
+                }
+            }
+            else if(_timeSinceLastSawPlayer < _suspicionWaitTime)
+            {
+                SuspicionBehavior();
+            }
+            else
+            {
+                PatrolBehavior();
+            }
+
+            UpdateTimers();
+        }
+
+        private void UpdateTimers()
+        {
+            _timeSinceLastSawPlayer += Time.deltaTime;
+            _timeAtWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehavior()
+        {
+            Vector3 nextPosition = _guardPosition;
+            if (_patrolPath != null)
+            {
+               
+                if (AtWaypoint())
+                {
+                    _timeAtWaypoint = 0;
+                    
+                    CycleWaypoint();
+                }
+
+                nextPosition = GetCurrentWaypoint();
+               
+                
+                if (_timeAtWaypoint > _waypointDwellTime)
+                {
+                    _mover.StartMoveAction(nextPosition);
                 }
             }
             else
             {
-                _mover.StartMoveAction(_guardPosition);
+                _mover.StartMoveAction(nextPosition);
             }
+           
+        }
+
+        private bool AtWaypoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(_transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < _waypointTolerance;
+        }
+
+        private void CycleWaypoint()
+        {
+           _currentWaypointIndex =  _patrolPath.GetNextIndex(_currentWaypointIndex);
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return _patrolPath.GetWaypoint(_currentWaypointIndex);
+        }
+
+        private void SuspicionBehavior()
+        {
+            _actionScheduler.CancelAction();
+        }
+
+        private void AttackBehavior()
+        {
+            _timeSinceLastSawPlayer = 0;
+            _fighter.Attack(_target);
         }
 
         private bool IsInRange(GameObject target)
---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,35 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private const float _waypointGizmoRadius = .25f;
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(GetWaypoint(i),_waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+            }
+        }
+
+        public int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+
+            return i + 1;
+        }
+        public Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+}
+

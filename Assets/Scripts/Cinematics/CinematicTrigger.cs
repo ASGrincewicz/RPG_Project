d56f6a6515561855008329345178c6cf974e78ba
Aaron S Grincewicz <veganimus@outlook.com>
2022-08-08 19:13:52 -0400

    Cinematic setup

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}

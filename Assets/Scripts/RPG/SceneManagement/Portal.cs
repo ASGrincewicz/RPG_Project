d5da4d27b1f02a2f5139cf1b6bfe326ec7187921
Aaron S Grincewicz <veganimus@outlook.com>
2022-08-10 23:11:56 -0400

    Portal Connections

--- Assets/Scripts/RPG/SceneManagement/Portal.cs
+++ Assets/Scripts/RPG/SceneManagement/Portal.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        public enum DestinationIdentifier
+        {
+            A,B,C,D,E,F,G,H
+        }
         [SerializeField] private int _sceneToLoad;
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private DestinationIdentifier _destination;
+
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) return;
@@ -16,10 +24,37 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (_sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set.");
+                yield break;
+            }
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(_sceneToLoad);
-            print("Scene Loaded");
+            Portal exitPortal = GetExitPortal();
+            UpdatePlayer(exitPortal);
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal portal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(portal._spawnPoint.position);
+            player.transform.rotation = portal._spawnPoint.rotation;
+        }
+
+        private Portal GetExitPortal()
+        {
+            Portal[] portals = FindObjectsOfType<Portal>();
+            foreach (Portal portal in portals)
+            {
+                if(portal == this) continue;
+                if (portal._destination == _destination)
+                {
+                    return portal;
+                }
+            }
+            return null;
         }
     }
 }

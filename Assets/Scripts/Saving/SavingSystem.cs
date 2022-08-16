7a1fa14cdaa7c8fc02baf9a3899512b471a0bd67
Aaron S Grincewicz <veganimus@outlook.com>
2022-08-15 23:56:31 -0400

    Save system setup

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,7 +1,47 @@
-﻿namespace Saving
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using UnityEngine;
+
+namespace Saving
 {
-    public class SavingSystem
+    public class SavingSystem: MonoBehaviour
     {
-        
+        public void Save(string savefile)
+        {
+            string path = GetPathFromSaveFile(savefile);
+            Debug.Log($"Saving to {path}");
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                Transform playerTransform = GetPlayerTransform();
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 position = new SerializableVector3(playerTransform.position);
+                formatter.Serialize(stream, position);
+            }
+        }
+
+        public void Load(string savefile)
+        {
+            string path = GetPathFromSaveFile(savefile);
+            using (FileStream stream = File.Open(path,FileMode.Open))
+            {
+                Transform playerTransform = GetPlayerTransform();
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 position = (SerializableVector3)formatter.Deserialize(stream);
+                playerTransform.position = position.ToVector();
+                print(playerTransform.position);
+            }
+        }
+
+        private string GetPathFromSaveFile(string savefile)
+        {
+            return Path.Combine($"{Application.persistentDataPath}",$"{savefile}.sav");
+        }
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindGameObjectWithTag("Player").transform;
+        }
     }
 }
--- Assets/Scripts/Saving/SavingWrapper.cs
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -1,7 +1,23 @@
-﻿namespace Saving
+﻿using System;
+using UnityEngine;
+
+namespace Saving
 {
-    public class SavingWrapper
+    public class SavingWrapper: MonoBehaviour
     {
-        
+        private const string _defaultSaveFile = "RPG_Save";
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                GetComponent<SavingSystem>().Save(_defaultSaveFile);
+            }
+
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                GetComponent<SavingSystem>().Load(_defaultSaveFile);
+            }
+        }
     }
 }
